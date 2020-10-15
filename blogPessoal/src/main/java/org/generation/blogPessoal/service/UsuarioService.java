3608ae6abdfffa1da94b3f4bef26008de07cc28c
Matheus Bello <mathbello@gmail.com>
2020-10-15 12:04:58 -0300

    Add security e service

---
+++ blogPessoal/src/main/java/org/generation/blogPessoal/service/UsuarioService.java
@@ -0,0 +1,48 @@
+package org.generation.blogPessoal.service;
+
+import java.nio.charset.Charset;
+import java.util.Optional;
+
+import org.apache.commons.codec.binary.Base64;
+import org.generation.blogPessoal.model.UserLogin;
+import org.generation.blogPessoal.model.Usuario;
+import org.generation.blogPessoal.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioService {
+
+	@Autowired
+	private UsuarioRepository repository;
+	
+	public Usuario cadastrarUsuario (Usuario user) {
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		
+		String senhaEncoder = encoder.encode(user.getSenha());
+		user.setSenha(senhaEncoder);
+		
+		return repository.save(user);
+	}
+	
+	public Optional<UserLogin> logar(Optional<UserLogin> user) {
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		Optional<Usuario> usuario = repository.findByUser(user.get().getUser());
+		
+		if(usuario.isPresent()) {
+			if(encoder.matches(user.get().getSenha(), usuario.get().getSenha())) {
+				String auth = user.get().getUser() + ": " + user.get().getSenha();
+				byte[] encodeAuth = Base64.encodeBase64(auth.getBytes(Charset.forName("US-ASCII")));
+				String authHeader = "Basic " + new String(encodeAuth);
+				
+				user.get().setToken(authHeader);
+				user.get().setNome(usuario.get().getNome());
+				
+				return user;
+			}
+		}
+		
+		return null;
+	}
+}
